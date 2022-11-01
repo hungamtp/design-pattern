945cff90736e888e1a2f89bf514021a052946b66
hungnguyenba <hung.nguyenba@saigontechnology.com>
2022-11-01 17:32:59 +0700

    EagerInitializedSingleton

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.Normalizer;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String réult = "Đi đĩ đì đi đâu con lâu mơ nói";
+        String keyword = "di";
+        System.out.println(StringUtils.stripAccents("Tĥïŝ ĩš â fůňķŷ Šťŕĭńġ"));
+        System.out.println(Normalizer.isNormalized("āăąēîïĩíĝġńñšŝśûůŷ", Normalizer.Form.NFKD));
     }
 }
