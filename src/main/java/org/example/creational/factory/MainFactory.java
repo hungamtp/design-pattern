fbf33eefe4ecd564cb1c9315fe33565c5d9522a6
Hung <hungamtp@gmail.com>
2023-12-22 13:20:53 +0700

    adapter

--- src/main/java/org/example/creational/factory/MainFactory.java
+++ src/main/java/org/example/creational/factory/MainFactory.java
@@ -4,7 +4,8 @@ public class MainFactory {
     public static void main(String[] args) {
         People nurse = PeopleFactory.getComputer("N" , "Nurse" , 14);
         People doctor = PeopleFactory.getComputer("D" , "Doctor" , 14);
-        System.out.println(doctor.toString());
-        System.out.println(nurse.toString());
+        System.out.println(doctor);
+        System.out.println(nurse);
+        var a = 5;
     }
 }
