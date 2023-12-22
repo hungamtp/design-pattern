1a0fa7e634c1106e96ef68d3f92f65aa9f371ad3
Hung <hungamtp@gmail.com>
2023-12-22 13:48:54 +0700

    flyweight design pattern

---
+++ src/main/java/org/example/structural/flyweight/Shape.java
@@ -0,0 +1,9 @@
+package org.example.structural.flyweight;
+
+import java.awt.Graphics;
+import java.awt.Color;
+
+public interface Shape {
+    public void draw(Graphics g, int x, int y, int width, int height,
+                     Color color);
+}
---
+++ src/main/java/org/example/structural/flyweight/readme.md
@@ -0,0 +1,3 @@
+## Flyweight design pattern 
+# use case: when the program need create huge amount of instanes
+reuse instances by setting new attributes and need no creation
